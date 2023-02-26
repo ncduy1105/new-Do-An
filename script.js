2c8a934d101aa1b3f8cc6f888ebf8fc1c5f78515
unknown <ncduy1105@gmail.com>
2023-02-26 08:18:52 +0700

    Create git Do An

---
+++ script.js
@@ -0,0 +1,345 @@
+var myIndex = 0;
+slide();
+
+    function slide() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow1");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(slide, 1000); 
+    }
+
+var myIndex2=0;
+slide2();
+
+    function slide2() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow2");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex2++;
+        if (myIndex2 > x.length) {myIndex2 = 1}    
+        x[myIndex2-1].style.display = "block";  
+        setTimeout(slide2, 1000); 
+    }
+
+var myIndex3=0;
+slide3();
+
+    function slide3() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow3");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex3++;
+        if (myIndex3 > x.length) {myIndex3 = 1}    
+        x[myIndex3-1].style.display = "block";  
+        setTimeout(slide3, 1000); 
+    }
+
+var myIndex4=0;
+slide4();
+
+    function slide4() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow4");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex4++;
+        if (myIndex4 > x.length) {myIndex4 = 1}    
+        x[myIndex4-1].style.display = "block";  
+        setTimeout(slide4, 1000); 
+    }
+
+var myIndex5=0;
+slide5();
+
+    function slide5() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow5");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex5++;
+        if (myIndex5 > x.length) {myIndex5 = 1}    
+        x[myIndex5-1].style.display = "block";  
+        setTimeout(slide5, 1000); 
+    }
+    
+var myIndex6=0;
+slide6();
+
+    function slide6() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow6");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex6++;
+        if (myIndex6 > x.length) {myIndex6 = 1}    
+        x[myIndex6-1].style.display = "block";  
+        setTimeout(slide6, 1000); 
+    }
+
+var myIndex7=0;
+slide7();
+
+    function slide7() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow7");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex7++;
+        if (myIndex7 > x.length) {myIndex7 = 1}    
+        x[myIndex7-1].style.display = "block";  
+        setTimeout(slide7, 1000); 
+    }
+
+var myIndex8=0;
+slide8();
+
+    function slide8() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow8");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex8++;
+        if (myIndex8 > x.length) {myIndex8 = 1}    
+        x[myIndex8-1].style.display = "block";  
+        setTimeout(slide8, 1000); 
+    }
+    
+var myIndex9=0;
+slide9();
+
+    function slide9() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow9");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex9++;
+        if (myIndex9 > x.length) {myIndex9 = 1}    
+        x[myIndex9-1].style.display = "block";  
+        setTimeout(slide9, 1000); 
+    }
+    
+var myIndex10=0;
+slide10();
+
+    function slide10() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow10");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex10++;
+        if (myIndex10 > x.length) {myIndex10 = 1}    
+        x[myIndex10-1].style.display = "block";  
+        setTimeout(slide10, 1000); 
+    }
+
+var myIndex11=0;
+slide11();
+
+    function slide11() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow11");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex11++;
+        if (myIndex11 > x.length) {myIndex11 = 1}    
+        x[myIndex11-1].style.display = "block";  
+        setTimeout(slide11, 1000); 
+    }
+var myIndex12=0;
+slide12();
+
+    function slide12() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow12");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex12++;
+        if (myIndex12 > x.length) {myIndex12 = 1}    
+        x[myIndex12-1].style.display = "block";  
+        setTimeout(slide12, 1000); 
+    }
+
+var myIndex13=0;
+slide13();
+
+    function slide13() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow13");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex13++;
+        if (myIndex13 > x.length) {myIndex13 = 1}    
+        x[myIndex13-1].style.display = "block";  
+        setTimeout(slide13, 1000); 
+    }
+
+var myIndex14=0;
+slide14();
+
+    function slide14() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow14");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex14++;
+        if (myIndex14 > x.length) {myIndex14 = 1}    
+        x[myIndex14-1].style.display = "block";  
+        setTimeout(slide14, 1000); 
+    }
+
+var myIndex15=0;
+slide15();
+
+    function slide15() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow15");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex15++;
+        if (myIndex15 > x.length) {myIndex15 = 1}    
+        x[myIndex15-1].style.display = "block";  
+        setTimeout(slide15, 1000); 
+    }
+
+var myIndex16=0;
+slide16();
+
+    function slide16() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow16");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex16++;
+        if (myIndex16 > x.length) {myIndex16 = 1}    
+        x[myIndex16-1].style.display = "block";  
+        setTimeout(slide16, 1000); 
+    }
+
+var myIndex17=0;
+slide17();
+
+    function slide17() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow17");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex17++;
+        if (myIndex17 > x.length) {myIndex17 = 1}    
+        x[myIndex17-1].style.display = "block";  
+        setTimeout(slide17, 1000); 
+    }
+
+var myIndex18=0;
+slide18();
+
+    function slide18() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow18");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex18++;
+        if (myIndex18 > x.length) {myIndex18 = 1}    
+        x[myIndex18-1].style.display = "block";  
+        setTimeout(slide18, 1000); 
+    }
+
+var myIndex19=0;
+slide19();
+
+    function slide19() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow19");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex19++;
+        if (myIndex19 > x.length) {myIndex19 = 1}    
+        x[myIndex19-1].style.display = "block";  
+        setTimeout(slide19, 1000); 
+    }
+
+var myIndex20=0;
+slide20();
+
+    function slide20() 
+    {
+        var i;
+        var x = document.getElementsByClassName("box_slideshow20");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex20++;
+        if (myIndex20 > x.length) {myIndex20 = 1}    
+        x[myIndex20-1].style.display = "block";  
+        setTimeout(slide20, 1000); 
+    }
+
+
+    let mybutton = document.getElementById("myBtn");
+
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+    
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+
+    function language() {
+        var x = document.getElementById("div1");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+        } else {
+          x.style.display = "none";
+        }
+      }
